272989c4b2b72db1721bc23fcd8cb63ca76f5e79
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-06-09 23:24:31 -0300

    Corrigindo erro

--- projeto-java-teste/src/main/java/classeMain/ClasseMain1.java
+++ projeto-java-teste/src/main/java/classeMain/ClasseMain1.java
@@ -5,7 +5,12 @@ public class ClasseMain1 {
 	public static void main(String[] args) {
 
 		System.out.println("Olá Java");
+<<<<<<< main
 		System.out.println("Olá Java Jdev Treinamento");
+		System.out.println("Testando configuração git");
+		System.out.println("Testando configuração git com ssh");
+=======
+>>>>>>> 4ec2a63 Commit de exemplo de aula do git. Adicionei a linha que imprime msg de ola java.
 
 	}
 
