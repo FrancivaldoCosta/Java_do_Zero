77b1c79840a90520207427c8d74b0551f4db483d
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-07-15 19:20:23 -0300

    Atualizando

--- projeto-java-teste/src/main/java/m04_estruturas_condicionais_e_tomada_de_decisao_no_java/ex01.java
+++ projeto-java-teste/src/main/java/m04_estruturas_condicionais_e_tomada_de_decisao_no_java/ex01.java
@@ -1,5 +1,35 @@
 package m04_estruturas_condicionais_e_tomada_de_decisao_no_java;
 
 public class ex01 {
+	
+
+	public static void main(String[] args) {
+		
+		
+		
+		int idade = 17;
+		
+		if (idade >= 18) {  /* true e false */
+			System.out.println("Maior de idade"); /* Executa se for true */
+		} else {
+			System.out.println("Menor de idade"); /* Executa se for false */
+		}
+		
+		boolean usuarioAtivo = true;
+		
+		if (usuarioAtivo) {
+			System.out.println("Usuário ativo");
+		} else {
+			System.out.println("Usuário não está ativo");
+		}
+		
+		double saldoSalario = 3500;
+		
+		if (saldoSalario >= 3500) {
+			System.out.println("Seu saldo é igual ou acima de 3500");
+		} else {
+			System.out.println("Seu saldo é menor que 3500");
+		}
+	}
 
 }
---
+++ projeto-java-teste/src/main/java/m04_estruturas_condicionais_e_tomada_de_decisao_no_java/ex02.java
@@ -0,0 +1,10 @@
+package m04_estruturas_condicionais_e_tomada_de_decisao_no_java;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
