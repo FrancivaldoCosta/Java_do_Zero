55c2a804ef4837d6993433becbc54d7e4d7d8e88
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-07-16 01:32:09 -0300

    Aula 11.2

--- projeto-java-teste/src/main/java/m04_estruturas_condicionais_e_tomada_de_decisao_no_java/ex02.java
+++ projeto-java-teste/src/main/java/m04_estruturas_condicionais_e_tomada_de_decisao_no_java/ex02.java
@@ -3,8 +3,36 @@ package m04_estruturas_condicionais_e_tomada_de_decisao_no_java;
 public class ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double nota = 7.9;
+		
+		
+		if (nota >= 8.9) {
+			System.out.println("Excelente, aprovado");
+		} else if (nota >= 7.0) {
+			System.out.println("Voçê está aprovado");
+		} else if (nota >= 5.0) {
+			System.out.println("Voçê está em recuperação");
+		} else {
+			System.out.println("Voçê está reprovado");
+		}
+		
+		
+		/*s==================ERRO COMUM DE NÃO USAR ELSE gera bugs e execução indevida==================*/
+	
+				if (nota >= 8.9) {
+					System.out.println("Excelente, aprovado");
+				} else if (nota >= 7.0) {
+					System.out.println("Voçê está aprovado");
+				} else if (nota >= 5.0) {
+					System.out.println("Voçê está em recuperação");
+				} else {
+					System.out.println("Voçê está reprovado");
+				}
+					
+				
+				
+				
 	}
 
 }
