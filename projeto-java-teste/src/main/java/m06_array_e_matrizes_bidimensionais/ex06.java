8f2071c546735b7a03ae1ffc47cc328a05e87094
Francivaldo Costa <francivaldoaccosta@gmail.com>
2026-09-01 20:55:19 -0300

    Continuando aulas

--- projeto-java-teste/src/main/java/m06_array_e_matrizes_bidimensionais/ex06.java
+++ projeto-java-teste/src/main/java/m06_array_e_matrizes_bidimensionais/ex06.java
@@ -5,12 +5,41 @@ public class ex06 {
 	public static void main(String[] args) {
 
 		int[] notas = new int[4];
-		notas[0] = 50;
+		notas[0] = 90;
 		notas[1] = 40;
-		notas[2] = 66;
-		notas[3] = 75;
+		notas[2] = 75;
+		notas[3] = 66;
 		
+		/* Qual será a maior nota ou número? */
+		
+				
+		int maior = notas[0];
+		
+		for (int n : notas) {
+			
+			if (n > maior) {
+				maior = n;
+			}
+			
+		}
+		
+		System.out.println("A maior nota é: " + maior);
+		
+		
+		
+		
+		int menor = notas[0];
+	
+	for (int n : notas) {
+		
+		if (n < menor) {
+			menor = n;
+		}
 		
 	}
+	
+	System.out.println("A menor nota é: " + menor);
+	
+}
 
 }
